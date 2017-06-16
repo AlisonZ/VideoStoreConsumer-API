cf2b2d27c89ad031aa47591a23ebd2d5c775864f
add2point71dots <alcland@gmail.com>
2017-06-16 10:55:55 -0700

    cleaned up image_url method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,12 +10,9 @@ class Movie < ApplicationRecord
 
   def image_url
       orig_value = read_attribute :image_url
-    #   why doesn't external work for the elsif condition??????
-    #   external = read_attribute :external_id
       if !orig_value
           MovieWrapper::DEFAULT_IMG_URL
-    #   elsif external == null
-      elsif orig_value[0] == "/"
+      elsif external_id
           MovieWrapper.construct_image_url(orig_value)
       else
           orig_value
